cdde1f81c124004924b4fd1937d0519e0952d09b
guillemoulloa53636 <guillermoliceotecnico@gmail.com>
2026-09-02 19:25:40 -0400

    cuarto commit(desarrollo detalle)

--- main.js
+++ main.js
@@ -3,41 +3,41 @@ const productosJSON = {
         "titulo": "Pasaje Santiago - Valparaíso",
         "precio": "$10.000",
         "descripcion": "Viaje directo a Valparaíso. Salidas cada 15 minutos desde Terminal Alameda. Servicio clásico con asientos reclinables.",
-        "imagen": "Img/Buses.png",
+        "imagen": "Img/Valpo.webp",
         "breadcrumb": "Santiago a Valparaíso",
-        "galeria": ["Img/Buses.png", "Img/Buses.png", "Img/Buses.png"]
+        "galeria": ["Img/Valpo.webp", "Img/Buses.png"]
     },
     "laserena": {
         "titulo": "Pasaje Santiago - La Serena",
         "precio": "$18.500",
         "descripcion": "Recorrido hacia el norte chico con servicio Salón Cama, incluye snacks y puertos USB en cada asiento.",
-        "imagen": "Img/Buses.png",
+        "imagen": "Img/Serena.webp",
         "breadcrumb": "Santiago a La Serena",
-        "galeria": ["Img/Buses.png", "Img/Buses.png", "Img/Buses.png"]
+        "galeria": ["Img/Serena.webp"]
     },
     "concepcion": {
         "titulo": "Pasaje Santiago - Concepción",
         "precio": "$22.000",
         "descripcion": "Viaje nocturno o diurno directamente a la Región del Biobío. Buses equipados con climatizador de doble zona.",
-        "imagen": "Img/Buses.png",
+        "imagen": "Img/Gran_Concepcion.jpg",
         "breadcrumb": "Santiago a Concepción",
-        "galeria": ["Img/Buses.png", "Img/Buses.png", "Img/Buses.png"]
+        "galeria": ["Img/Gran_Concepcion.jpg"]
     },
     "pucon": {
         "titulo": "Pasaje Santiago - Pucón",
         "precio": "$28.000",
         "descripcion": "Ruta turística hacia la zona de lagos y volcanes. Asientos Cama Premium con almohada y frazada incluida.",
-        "imagen": "Img/Buses.png",
+        "imagen": "Img/Temuco.jpg",
         "breadcrumb": "Santiago a Pucón",
-        "galeria": ["Img/Buses.png", "Img/Buses.png", "Img/Buses.png"]
+        "galeria": ["Img/Temuco.jpg"]
     },
     "puertomontt": {
         "titulo": "Pasaje Santiago - Puerto Montt",
         "precio": "$35.000",
         "descripcion": "Servicio de larga distancia con máxima comodidad, pantalla individual de entretenimiento y asistencia a bordo.",
-        "imagen": "Img/Buses.png",
+        "imagen": "Img/Mont.jpg",
         "breadcrumb": "Santiago a Puerto Montt",
-        "galeria": ["Img/Buses.png", "Img/Buses.png", "Img/Buses.png"]
+        "galeria": ["Img/Mont.jpg"]
     }
 };
 
