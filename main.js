4582dbfc89aad6194053aae89eb0039ca0bba334
guillemoulloa53636 <guillermoliceotecnico@gmail.com>
2026-09-03 01:03:03 -0400

    Quinto Commit(iDetalle viajes y mejoras generales)

--- main.js
+++ main.js
@@ -2,43 +2,100 @@ const productosJSON = {
     "valparaiso": {
         "titulo": "Pasaje Santiago - Valparaíso",
         "precio": "$10.000",
-        "descripcion": "Viaje directo a Valparaíso. Salidas cada 15 minutos desde Terminal Alameda. Servicio clásico con asientos reclinables.",
+        "descripcion": "Un viaje bohemio e histórico caracterizado por sus cerros llenos de murales, colorides arquitectónicas y miradores hacia el Pacífico. Destaca por el recorrido en sus tradicionales ascensores funiculares, las caminatas por los cerros Alegre y Concepción, y la visita a La Sebastiana, una de las casas-museo de Pablo Neruda.",
         "imagen": "Img/Valpo.webp",
         "breadcrumb": "Santiago a Valparaíso",
-        "galeria": ["Img/Valpo.webp", "Img/Buses.png"]
+        "galeria": ["Img/Valpo.webp", "Img/valpo2.jpg"]
     },
     "laserena": {
         "titulo": "Pasaje Santiago - La Serena",
-        "precio": "$18.500",
-        "descripcion": "Recorrido hacia el norte chico con servicio Salón Cama, incluye snacks y puertos USB en cada asiento.",
+        "precio": "$22.500",
+        "descripcion": "Un destino costero de ritmo sereno, famoso por su arquitectura neocolonial, el icónico Faro Monumental y la extensa Avenida del Mar. Funciona además como la antesala hacia el Valle del Elqui, ideal para el astroturismo, la visita a observatorios y el turismo pisquero..",
         "imagen": "Img/Serena.webp",
         "breadcrumb": "Santiago a La Serena",
-        "galeria": ["Img/Serena.webp"]
+        "galeria": ["Img/Serena.webp","Img/faro.jpg"]
     },
     "concepcion": {
         "titulo": "Pasaje Santiago - Concepción",
         "precio": "$22.000",
-        "descripcion": "Viaje nocturno o diurno directamente a la Región del Biobío. Buses equipados con climatizador de doble zona.",
+        "descripcion": "La capital universitaria y del rock chileno. Combina una animada vida urbana con abundantes áreas verdes, destacando el icónico campus de la Universidad de Concepción, la Pinacoteca, el Parque Ecuador y la cercanía al histórico Monitor Huáscar en el puerto de Talcahuano..",
         "imagen": "Img/Gran_Concepcion.jpg",
         "breadcrumb": "Santiago a Concepción",
-        "galeria": ["Img/Gran_Concepcion.jpg"]
+        "galeria": ["Img/Gran_Concepcion.jpg","Img/Conce2.jpg"]
     },
     "pucon": {
         "titulo": "Pasaje Santiago - Pucón",
-        "precio": "$28.000",
-        "descripcion": "Ruta turística hacia la zona de lagos y volcanes. Asientos Cama Premium con almohada y frazada incluida.",
+        "precio": "$27.000",
+        "descripcion": "El epicentro del turismo de aventura en la Región de La Araucanía. Dominado por la figura del activo volcán Villarrica, ofrece trekking por el Parque Nacional Huerquehue, deportes náuticos en el lago de arena negra, rafting en el río Trancura y relajación en múltiples centros termales.",
         "imagen": "Img/Temuco.jpg",
         "breadcrumb": "Santiago a Pucón",
-        "galeria": ["Img/Temuco.jpg"]
+        "galeria": ["Img/Temuco.jpg","Img/Pucon2.jpg"]
     },
     "puertomontt": {
         "titulo": "Pasaje Santiago - Puerto Montt",
-        "precio": "$35.000",
-        "descripcion": "Servicio de larga distancia con máxima comodidad, pantalla individual de entretenimiento y asistencia a bordo.",
+        "precio": "$30.000",
+        "descripcion": "La puerta de entrada a la Patagonia chilena y la Región de Los Lagos. Destaca por su patrimonio marítimo y gastronómico en la caleta Angelmó, sus vistas hacia el seno de Reloncaví, y su valor como punto de conexión hacia la Isla Grande de Chiloé y la Carretera Austral..",
         "imagen": "Img/Mont.jpg",
         "breadcrumb": "Santiago a Puerto Montt",
-        "galeria": ["Img/Mont.jpg"]
+        "galeria": ["Img/Mont.jpg","Img/Mont2.jpg"]
+    },
+    "chiloe": {
+        "titulo": "Pasaje Santiago - chiloe",
+        "precio": "$25.000",
+        "descripcion": "Un viaje impregnado de mitología, verde paisaje insular y una identidad cultural única. Destaca por sus tradicionales palafitos de colores sobre el agua, sus históricas iglesias de madera reconocidas como Patrimonio de la Humanidad y su destacada gastronomía local como el curanto..",
+        "imagen": "Img/Chilote.webp",
+        "breadcrumb": "Santiago a chiloe",
+        "galeria": ["Img/Chilote.webp","Img/Chiloe2.jpg"]
+    },
+    "talca": {
+        "titulo": "Pasaje Santiago - talca",
+        "precio": "$20.000",
+        "descripcion": "Corazón histórico y administrativo de la Región del Maule. Sirve como punto de partida estratégico para explorar los valles vitivinícolas de la zona y los atractivos de la precordillera, entre los que destacan la Reserva Nacional Altos de Lircay y la ruta hacia la Laguna del Maule..",
+        "imagen": "Img/Talca.jpg",
+        "breadcrumb": "Santiago a talca",
+        "galeria": ["Img/Talca.jpg","Img/TalcaGod.jpg"]
+    },
+    "pichilemu": {
+        "titulo": "Pasaje Santiago - Pichilemu",
+        "precio": "$20.000",
+        "descripcion": "La capital chilena del surf, reconocida mundialmente por su ambiente relajado y sus impresionantes olas. Su lugar más emblemático es Punta de Lobos. Pichilemu, con sus característicos acantilados y roqueríos, además de contar con paseos familiares en el Parque Ross y playas aptas para el descanso.",
+        "imagen": "Img/Pichilemu.jpg",
+        "breadcrumb": "Santiago a Pichilemu",
+        "galeria": ["Img/Pichilemu.jpg","Img/Pichilemu2.jpg"]
+    },
+    "Quintero": {
+        "titulo": "Pasaje Santiago - Quintero",
+        "precio": "$15.000",
+        "descripcion": "Una tradicional ciudad costera de la zona central con numerosas playas y miradores naturales hacia el Pacífico. Ofrece un ritmo más pausado, ideal para caminatas junto al mar, visitas a sitios como la Cueva del Pirata y degustaciones de pescados y mariscos en sus caletas locales.",
+        "imagen": "Img/quintero.jpg",
+        "breadcrumb": "Santiago a Quintero",
+        "galeria": ["Img/quintero.jpg","Img/quintero2.jpg"]
+    },
+    "Arica": {
+        "titulo": "Pasaje Santiago - Arica",
+        "precio": "$35.000",
+        "descripcion": "La Ciudad de la Eterna Primavera, famosa por su clima templado durante todo el año y su profunda riqueza precolombina. Su símbolo geográfico e histórico más importante es el imponente Morro de Arica, que ofrece vistas panorámicas de la ciudad y el mar. Además, destaca por sus amplias balnearios costeros como Playa Chinchorro y por las excursiones hacia el Valle de Azapa, celebre por sus olivareños, geoglifos y el museo que resguarda las milenarias momias Chinchorro..",
+        "imagen": "Img/Arica.jpg",
+        "breadcrumb": "Santiago a Arica",
+        "galeria": ["Img/Arica.jpg","Img/Arica2.jpg"]
+    },
+    "Antofagasta": {
+        "titulo": "Pasaje Santiago - Antofagasta",
+        "precio": "$30.000",
+        "descripcion": "Conocida como la Perla del Norte, es una importante ciudad costera e industrial enmarcada por el desierto de Atacama. Su hito más emblemático es la impresionante formación rocosa del Mirador Monumento Natural La Portada, ubicada sobre el océano Pacífico. En el área urbana destacan los extensos paseos por su borde costero, el legado de la época salitrera en el céntrico Muelle Histórico y la cercanía a atractivos escultóricos del desierto.",
+        "imagen": "Img/Antofa.jpg",
+        "breadcrumb": "Santiago a Antofagasta",
+        "galeria": ["Img/Antofa.jpg","Img/Anto2.jpg"]
+    },
+    "Viña": {
+        "titulo": "Pasaje Santiago - Vinia",
+        "precio": "$15.000",
+        "descripcion": "La popular Ciudad Jardín, reconocida por su amplia infraestructura turística, paseos en el borde costero y parques urbanos. Entre sus íconos se encuentran el Reloj de Flores de Viña del Mar, el palacio y parque de la Quinta Vergara, y sus animadas zonas gastronómicas y nocturnas.",
+        "imagen": "Img/Vinia.jpg",
+        "breadcrumb": "Santiago a Vinia",
+        "galeria": ["Img/Vinia.jpg","Img/Flores.jpg"]
     }
+    
 };
 
 // Función que actualiza la interfaz al hacer clic en 'Ver más'
@@ -54,11 +111,17 @@ function cambiarProducto(idProducto) {
 
         const galleryContainer = document.getElementById('product-gallery');
         galleryContainer.innerHTML = '';
+        
 
         producto.galeria.forEach((imgUrl, index) => {
+           if (!imgUrl) return; 
+
             const borderClass = index === 0 ? 'border-primary' : 'opacity-75';
             galleryContainer.innerHTML += `
-                <button class="btn p-0 border border-2 ${borderClass} rounded-3 overflow-hidden" style="width: 80px; height: 80px;">
+                <button type="button" 
+                        onclick="cambiarImagenPrincipal('${imgUrl}')" 
+                        class="btn p-0 border border-2 ${borderClass} rounded-3 overflow-hidden" 
+                        style="width: 80px; height: 80px;">
                     <img src="${imgUrl}" class="w-100 h-100 object-fit-cover" alt="Vista ${index + 1}">
                 </button>
             `;
@@ -89,6 +152,40 @@ document.getElementById('formLogin').addEventListener('submit', function(event) 
         window.location.href = "index.html";
     } else {
         // Muestra el mensaje de error en pantalla
+        errorMensaje.classList.remove('d-none');
+    }
+});
+
+function cambiarImagenPrincipal(src) {
+    document.getElementById('product-img').src = src;
+}
+
+document.getElementById('formLogin').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Configuración de reglas en formato JSON
+    const configValidacion = {
+        "dominiosPermitidos": [
+            "@gmail.com",
+            "@duoc.cl",
+            "@profesor.duoc.cl"
+        ]
+    };
+
+    const inputEmail = document.getElementById('rutOEmail').value.trim().toLowerCase();
+    const errorMensaje = document.getElementById('errorMensaje');
+
+    // Valida que exista texto antes del '@' (ej: 'juan') y que termine en un dominio permitido
+    const esValido = configValidacion.dominiosPermitidos.some(dominio => {
+        const tieneTextoAntesDelAt = inputEmail.indexOf('@') > 0;
+        return tieneTextoAntesDelAt && inputEmail.endsWith(dominio);
+    });
+
+    if (esValido) {
+        errorMensaje.classList.add('d-none');
+        window.location.href = "Menu.html"; // Redirige a la siguiente vista
+    } else {
+        errorMensaje.innerText = "Ingresa un correo válido (ej: juan@gmail.com, usuario@duoc.cl)";
         errorMensaje.classList.remove('d-none');
     }
 });
